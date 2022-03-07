57eff3a9c0a84815e38f054e60d04cde5f638494
ben_astengo <astengo93@gmail.com>
2022-03-07 15:22:26 -0600

    Ben

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <P>HI THERE</P>
+    <P>HI THERE, FROM BEN</P>
     <p>
       For a guide and recipes on how to configure / customize this project,
       <br />
