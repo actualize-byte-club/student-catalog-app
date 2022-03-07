6829e560bca4719d587121cd68e607b7df55e571
kevin <kg1941a@gmail.com>
2022-03-07 16:21:45 -0500

    adds wee woos

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,6 +2,7 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <P>HI THERE</P>
+    <p>wee woo wee woo</p>
     <p>
       For a guide and recipes on how to configure / customize this project,
       <br />
