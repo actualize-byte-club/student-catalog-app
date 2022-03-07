0476136e01c12a6e1b34e0237f833904386c4227
Camden <camdencparker@gmail.com>
2022-03-07 14:44:56 -0700

    adds StudentsIndex and StudentsShow views and routes

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,8 @@
     <router-link to="/">Home</router-link>
     |
     <router-link to="/about">About</router-link>
+    |
+    <router-link to="/students">Index</router-link>
   </div>
   <router-view />
 </template>
