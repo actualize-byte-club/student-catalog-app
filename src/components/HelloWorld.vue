bfc3fffcb92b11cf45d23a9968d43941e4bf12ca
Meghan Scherzer <meghan.scherzer@gmail.com>
2022-03-07 15:28:18 -0600

    meghan rocks

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,7 +5,7 @@
     <p>wee woo wee woo</p>
     <P>HI THERE, FROM BEN</P>
     <p>
-      For a guide and recipes on how to configure / customize this project,
+      Meghan Rocks
       <br />
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>
