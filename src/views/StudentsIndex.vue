9adaa54cec6bfb00d2538ac277d362590fd64b15
ben_astengo <astengo93@gmail.com>
2022-03-09 15:58:13 -0600

    Adds clickable link to image routing to show page

--- src/views/StudentsIndex.vue
+++ src/views/StudentsIndex.vue
@@ -3,157 +3,11 @@ import axios from "axios";
 export default {
   data: function () {
     return {
-      students: [
-        {
-          id: 1,
-          first_name: "Ben",
-          last_name: "Astengo",
-          capstone: [
-            {
-              title: "SprintRoute",
-              image:
-                "https://komonews.com/resources/media/dfb64beb-1311-490c-a8e1-0b7dda676807-medium16x9_everett_crash_komo_07.jpg?1460578767600",
-            },
-            { title: "Something", image: "Somethingelseeee" },
-          ],
-        },
-        {
-          id: 2,
-          first_name: "Camden",
-          last_name: "Parker",
-          capstone: [
-            {
-              title: "NeedABike",
-              image: "https://i.pinimg.com/originals/c3/02/af/c302afefbc869048ce8cf9aeeac10338.jpg",
-            },
-          ],
-        },
-        {
-          id: 2,
-          first_name: "Camden",
-          last_name: "Parker",
-          capstone: [
-            {
-              title: "NeedABike",
-              image: "https://i.pinimg.com/originals/c3/02/af/c302afefbc869048ce8cf9aeeac10338.jpg",
-            },
-          ],
-        },
-        {
-          id: 2,
-          first_name: "Camden",
-          last_name: "Parker",
-          capstone: [
-            {
-              title: "NeedABike",
-              image: "https://i.pinimg.com/originals/c3/02/af/c302afefbc869048ce8cf9aeeac10338.jpg",
-            },
-          ],
-        },
-        {
-          id: 2,
-          first_name: "Camden",
-          last_name: "Parker",
-          capstone: [
-            {
-              title: "NeedABike",
-              image: "https://i.pinimg.com/originals/c3/02/af/c302afefbc869048ce8cf9aeeac10338.jpg",
-            },
-          ],
-        },
-        {
-          id: 2,
-          first_name: "Camden",
-          last_name: "Parker",
-          capstone: [
-            {
-              title: "NeedABike",
-              image: "https://i.pinimg.com/originals/c3/02/af/c302afefbc869048ce8cf9aeeac10338.jpg",
-            },
-          ],
-        },
-        {
-          id: 2,
-          first_name: "Camden",
-          last_name: "Parker",
-          capstone: [
-            {
-              title: "NeedABike",
-              image: "https://i.pinimg.com/originals/c3/02/af/c302afefbc869048ce8cf9aeeac10338.jpg",
-            },
-          ],
-        },
-        {
-          id: 2,
-          first_name: "Camden",
-          last_name: "Parker",
-          capstone: [
-            {
-              title: "NeedABike",
-              image: "https://i.pinimg.com/originals/c3/02/af/c302afefbc869048ce8cf9aeeac10338.jpg",
-            },
-          ],
-        },
-        {
-          id: 2,
-          first_name: "Camden",
-          last_name: "Parker",
-          capstone: [
-            {
-              title: "NeedABike",
-              image: "https://i.pinimg.com/originals/c3/02/af/c302afefbc869048ce8cf9aeeac10338.jpg",
-            },
-          ],
-        },
-        {
-          id: 2,
-          first_name: "Camden",
-          last_name: "Parker",
-          capstone: [
-            {
-              title: "NeedABike",
-              image: "https://i.pinimg.com/originals/c3/02/af/c302afefbc869048ce8cf9aeeac10338.jpg",
-            },
-          ],
-        },
-        {
-          id: 2,
-          first_name: "Camden",
-          last_name: "Parker",
-          capstone: [
-            {
-              title: "NeedABike",
-              image: "https://i.pinimg.com/originals/c3/02/af/c302afefbc869048ce8cf9aeeac10338.jpg",
-            },
-          ],
-        },
-        {
-          id: 2,
-          first_name: "Camden",
-          last_name: "Parker",
-          capstone: [
-            {
-              title: "NeedABike",
-              image: "https://i.pinimg.com/originals/c3/02/af/c302afefbc869048ce8cf9aeeac10338.jpg",
-            },
-          ],
-        },
-        {
-          id: 2,
-          first_name: "Camden",
-          last_name: "Parker",
-          capstone: [
-            {
-              title: "NeedABike",
-              image: "https://i.pinimg.com/originals/c3/02/af/c302afefbc869048ce8cf9aeeac10338.jpg",
-            },
-          ],
-        },
-      ],
+      students: {},
     };
   },
   created: function () {
-    // this.indexStudents();
+    this.indexStudents();
   },
   methods: {
     indexStudents: function () {
@@ -173,8 +27,16 @@ export default {
       <div class="col" v-for="student in students" v-bind:key="student.id">
         <div class="">
           <h2>{{ student.first_name }} {{ student.last_name }}</h2>
-          <h3>Capstone: {{ student.capstone[0].title }}</h3>
-          <router-link to="/students/"><img v-bind:src="student.capstone[0].image" /></router-link>
+          <h3>
+            Capstone:
+
+            <div v-for="capstone in student.combined_attributes.capstones" :key="capstone.id">
+              {{ capstone.name }}
+              <router-link to="/students/">
+                <img v-bind:src="capstones.image" />
+              </router-link>
+            </div>
+          </h3>
         </div>
       </div>
     </div>
